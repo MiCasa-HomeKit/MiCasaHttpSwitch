3e4838a0534381aeadf1e98d2ea26b3bd4f9b232
Thomas Bonk <thomas@meandmymac.de>
2020-11-03 19:25:49 +0100

    - added config file - adjusted macOS install script

--- install-macos.sh
+++ install-macos.sh
@@ -5,5 +5,5 @@ swift build -c release
 
 echo "Installing to /usr/local..."
 mkdir -p /usr/local/lib/micasa
-cp .build/x86_64-unknown-linux-gnu/release/libMiCasaHttpSwitch.so /usr/local/lib/micasa/libMiCasaHttpSwitch.mcplugin
+cp  .build/x86_64-apple-macosx/release/libMiCasaHttpSwitch.dylib /usr/local/lib/micasa/libMiCasaHttpSwitch.mcplugin
 
